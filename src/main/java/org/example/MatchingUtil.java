33b1ec268a371abbad9066499c14b1d5c150580c
Utsav Mori <utsavrmori@gmail.com>
2021-04-14 16:26:34 +0530

    updated matchingutil

--- src/main/java/org/example/MatchingUtil.java
+++ src/main/java/org/example/MatchingUtil.java
@@ -1,18 +1,14 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
 
-public class MatchingUtil {
+public class MatchingUtil
+{
     private HashSet<String> list = new HashSet<>();
 
-
-
-    public MatchingUtil read(String pathName) {
+    public MatchingUtil read(String pathName)
+    {
         try (Scanner s = new Scanner(new File(pathName))) {
             while (s.hasNextLine()) {
                 list.add(filterLine(s.nextLine()));
@@ -23,23 +19,35 @@ public class MatchingUtil {
         return this;
     }
 
-    public String match(String pathName) {
-        StringBuilder builder= new StringBuilder();
+    public String match(String pathName, boolean returnOnlyNonMatched)
+    {
+        StringBuilder builder = new StringBuilder();
         try (Scanner s = new Scanner(new File(pathName))) {
             while (s.hasNextLine()) {
-                String line=filterLine(s.nextLine());
-                if(list.contains(line)){
+                String line = filterLine(s.nextLine());
+                if (list.contains(line) && !returnOnlyNonMatched) {
+                    list.remove(line);
                     builder.append(line).append(System.lineSeparator());
+                } else {
+                    if (returnOnlyNonMatched) {
+                        list.remove(line);
+                        builder.append(line).append(System.lineSeparator());
+                    }
+
                 }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        if (returnOnlyNonMatched) {
+            list.parallelStream().forEach(builder::append);
+        }
         return builder.toString();
 
     }
 
-    private String filterLine(String line) {
-        return Arrays.stream(line.trim().split(",")).map(String::trim).collect(Collectors.joining(","));
+    private String filterLine(String line)
+    {
+        return line.trim();
     }
 }
